439d5e041279f0905f4ce9b020e4cef7eab75014
Sn-Kinos <dony435@naver.com>
2021-09-09 22:35:49 +0900

    Design, 폰트 추가

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,17 +1,25 @@
 import GlobalStyle from '../styles/global'
 import Miu from '../styles/Miu'
+import MainFont from '../styles/MainFont'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { appWithTranslation } from 'next-i18next'
 import { ThemeProvider } from 'styled-components'
 import theme from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Miu />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>BlueArchive.is</title>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Miu />
+        <MainFont />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   )
 }
 
