ea6a9717c3e3de0e3739c2d352945d03bd3b056b
Sn-Kinos <dony435@naver.com>
2021-11-01 22:00:47 +0900

    Feat, 기존 사용하던 컴포넌트 추가

---
+++ web/components/styled/Article.ts
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+
+interface ArticleProps {
+  flexDirection?: string;
+  textAlign?: string;
+  height?: string;
+  justifyContent?: string;
+  alignItems?: string;
+}
+
+const Article = styled.article<ArticleProps>`
+  position: relative;
+  display: flex;
+  width: calc(100% - 116px);
+  max-width: 1324px;
+  height: ${(props) => props.height ?? '100%'};
+  margin: 0 auto;
+  text-align: ${(props) => props.textAlign ?? 'left'};
+  flex-direction: ${(props) => props.flexDirection ?? 'row'};
+  align-items: ${(props) => props.alignItems ?? 'flex-start'};
+  justify-content: ${(props) => props.justifyContent ?? 'flex-start'};
+
+  @media (max-width: 833px) {
+    width: calc(100% - 80px);
+  }
+
+  @media (max-width: 640px) {
+    width: calc(100% - 40px);
+  }
+`;
+
+export default Article;
