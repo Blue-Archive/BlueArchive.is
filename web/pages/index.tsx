109e8efd4bf3f78ced434d98310010db9ab9653b
Sn-Kinos <dony435@naver.com>
2021-11-01 22:01:04 +0900

    Design, 배경 이미지 적용

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -3,26 +3,37 @@ import { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Header from '../components/Header';
+import Section from '../components/styled/Section';
+
+const Background = styled.div`
+  position: fixed;
+  inset: 0;
+  background: url('/images/index/bg.jpg') no-repeat center center/cover;
+  filter: blur(10px) brightness(80%);
+  transform: scale(1.05);
+`;
 
 const Main = styled.main`
   display: flex;
   width: 100%;
   height: 100vh;
-  background-color: #273869;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
 `;
 
 const Home: NextPage = (): JSX.Element => {
   return (
     <>
+      <Background />
       <Head>
         <title>BlueArchive.is</title>
       </Head>
       <Header />
       <Main>
+        <Section></Section>
         <Image
-          src="/images/bluearchiveis_global.png"
+          src="/images/logo_global.png"
           alt="BlueArchive.is"
           width={708}
           height={188}
