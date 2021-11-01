cf931a42d14bd2d3648a237b3328252e2cfd452c
Sn-Kinos <dony435@naver.com>
2021-11-01 22:00:47 +0900

    Feat, 기존 사용하던 컴포넌트 추가

---
+++ web/components/styled/Section.ts
@@ -0,0 +1,36 @@
+import styled from '@emotion/styled';
+
+interface SectionProps {
+  backgroundImage?: string;
+  backgroundColor?: string;
+  backgroundSize?: string;
+  height?: string;
+  minHeight?: string;
+  heightOverflow?: boolean;
+  overlayColor?: string;
+  justifyContent?: string;
+}
+
+const Section = styled.section<SectionProps>`
+  position: relative;
+  display: flex;
+  width: 100%;
+  ${(props) => (props.heightOverflow ? 'min-' : '')}height: ${(props) =>
+    props.height ?? 'auto'};
+  background-image: ${(props) =>
+      props.overlayColor
+        ? `linear-gradient(${props.overlayColor}, ${props.overlayColor}),`
+        : ''}
+    url(${(props) => props.backgroundImage ?? ''});
+  background-color: ${(props) => props.backgroundColor ?? '#fff'};
+  background-size: ${(props) => props.backgroundSize ?? 'cover'};
+  background-position: center;
+  margin: 0 auto;
+  overflow: hidden;
+  justify-content: ${(props) =>
+    props.heightOverflow ? 'flex-start' : 'center'};
+  flex-direction: column;
+`;
+
+export type { SectionProps };
+export { Section as default };
