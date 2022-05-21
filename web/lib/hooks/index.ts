c0f6aef62c509b84f9a67f2e2566f6b88c1a9a46
Sn-Kinos <dony435@naver.com>
2022-05-22 03:25:47 +0900

    fix, 존재하지 않는 파일 import 오류

--- web/lib/hooks/index.ts
+++ web/lib/hooks/index.ts
@@ -1,16 +1,10 @@
-import {
-  DefaultRootState,
-  shallowEqual,
-  TypedUseSelectorHook,
-  useDispatch,
-  useSelector,
-} from 'react-redux';
+import { shallowEqual, TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { AppDispatch, RootState } from '../store';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-export function useShallowSelector<TState = DefaultRootState, TSelected = unknown>(
+export function useShallowSelector<TState = unknown, TSelected = unknown>(
   selector: (state: TState) => TSelected
 ): TSelected {
   return useSelector(selector, shallowEqual);
